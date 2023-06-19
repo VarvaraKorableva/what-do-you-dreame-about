b773fe77c79838f45de7f4ce4b43fef666563d0b
Varvara Korableva <v.korableva1990@yandex.ru>
2023-06-19 13:17:04 +0300

    multer

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,6 @@
 const User = require('./models/user');
 require('dotenv').config();
-//import cors from 'cors';
 
-const fs = require('fs'); // Подключаем модуль fs для работы с файлами
-const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -13,30 +10,13 @@ const cors = require('cors');
 
 const { login, createUser, logout } = require('./controllers/users');
 const auth = require('./middlewares/auth');
-//const multer = require('./middlewares/multer');
 const NotFoundError = require('./Errors/NotFoundError');
 const handleErrors = require('./middlewares/errors');
 const { Reg } = require('./utils/const');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const multer = require('multer');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
-const storage = multer.diskStorage({
-  destination: function (_, __, cb) {
-    cb(null, 'uploads');
-  },
-  filename: function (_, file, cb) {
-    //cb(null, file.originalname);
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const extension = file.originalname.split('.').pop();
-    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension);
-  }
-});
-
-// Создание экземпляра multer с настройками хранилища
-const upload = multer({ storage });
 
 app.use(express.json());
 
@@ -63,6 +43,7 @@ app.get('/crash-test', () => {
     throw new Error('Сервер сейчас упадет');
   }, 0);
 });
+
 app.use('/uploads', express.static('uploads'));
 
 app.post('/signin', celebrate({
@@ -89,90 +70,6 @@ app.use(require('./routes/usersForAll'));
 app.use(require('./routes/myFriendDreams'));
 app.use(auth);
 
-//
-
-app.post('/upload', upload.single('image'), (req, res) => {
-  const userId = req.body.userId;
-  const imageBuffer = req.file.buffer;
-
-  User.findByIdAndUpdate(userId, { avatar: `/uploads/${req.file.filename}` }, { new: true })
-    .then((user) => {
-      res.send(user);
-    })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        res.status(400).send('Некорректный идентификатор пользователя');
-      } else {
-        res.status(500).send('Произошла ошибка при обновлении пользователя');
-      }
-    });
-});
-/*
-app.patch('/upload', upload.single('image'), (req, res) => {
-  const userId = req.body.userId;
-  const imageBuffer = req.file.buffer;
-
-  const filename = `${userId}_${Date.now()}.jpg`; // Генерируем уникальное имя файла
-  const filePath = path.join(__dirname, 'uploads', filename); // Путь к файлу
-
-  // Сохраняем файл на сервере
-  fs.writeFile(filePath, imageBuffer, (error) => {
-    if (error) {
-      console.error('Произошла ошибка при сохранении файла:', error);
-      res.status(500).send('Произошла ошибка при сохранении файла');
-      return;
-    }
-
-    // Обновляем пользователя в базе данных
-    User.findByIdAndUpdate(userId, { avatar: `/uploads/${filename}` }, { new: true })
-      .then((user) => {
-        res.send(user);
-      })
-      .catch((error) => {
-        if (error.name === 'CastError') {
-          res.status(400).send('Некорректный идентификатор пользователя');
-        } else {
-          res.status(500).send('Произошла ошибка при обновлении пользователя');
-        }
-      });
-  });
-});*/
-
-//рабочий
-app.patch('/upload', upload.single('image'), (req, res) => {
-  const userId = req.body.userId;
-  const imageBuffer = req.file.buffer;
-  
-  User.findByIdAndUpdate(userId, { avatar: `/uploads/${req.file.filename}` }, { new: true })
-    .then((user) => {
-      res.send(user);
-    })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        res.status(400).send('Некорректный идентификатор пользователя');
-      } else {
-        res.status(500).send('Произошла ошибка при обновлении пользователя');
-      }
-    });
-});
-/*
-app.patch('/upload', upload.single('image'), (req, res) => {
-  const userId = req.body.userId;
-  const imageBuffer = req.file.buffer;
-  
-  User.findByIdAndUpdate(userId, { avatar: imageBuffer }, { new: true })
-    .then((user) => {
-      res.send(user);
-    })
-    .catch((error) => {
-      if (error.name === 'CastError') {
-        res.status(400).send('Некорректный идентификатор пользователя');
-      } else {
-        res.status(500).send('Произошла ошибка при обновлении пользователя');
-      }
-    });
-});*/
-
 app.use(require('./routes/users'));
 app.use(require('./routes/dreams'));
 app.use(require('./routes/importantDates'));
@@ -183,14 +80,6 @@ app.all('*', () => {
   throw new NotFoundError('Страница не найдена');
 });
 
-
-//mongoose.set("strictQuery", true);
-/*
-mongoose.connect('mongodb://localhost:27017/dreamsAbout', 
-{ 
-  useNewUrlParser: true, 
-  family: 4 
-});*/
 mongoose 
  .connect('mongodb://localhost:27017/dreamsAbout', {
         useNewUrlParser: true,
--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,14 +1,21 @@
 const multer = require('multer');
 
-const uploadSingle = (req, res, next) => {
-  upload.single('file')(req, res, function (error) {
-    if (error instanceof multer.MulterError) {
-      // Обработка ошибки Multer
-      return res.status(400).json({ error: error.message });
-    } else if (error) {
-      // Обработка других ошибок
-      return res.status(500).json({ error: 'Server error' });
-    }
-    next();
-  });
-};
+const storage = multer.diskStorage({
+  destination: function (_, __, cb) {
+    cb(null, 'uploads');
+  },
+  filename: function (_, file, cb) {
+    //cb(null, file.originalname);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const extension = file.originalname.split('.').pop();
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + extension);
+  }
+});
+
+// Создание экземпляра multer с настройками хранилища
+const upload = multer({ storage });
+
+// Middleware для обработки загрузки файла
+const uploadMiddleware = upload.single('image');
+
+module.exports = uploadMiddleware;
